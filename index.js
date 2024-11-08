ac30214f11c20236abb6ba687440bb5b2fbccdb4
Alison Elvis <alisonelvis3@gmail.com>
2024-11-08 14:17:49 -0300

    Adicionado HTML e CSS

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static("public"));
 
-app.listen(3000, () => console.log('Servidor rodando em http://localhost:3000'));
+app.listen(PORT, () =>
+    console.log(`Servidor rodando em http://localhost:${PORT}`)
+);
